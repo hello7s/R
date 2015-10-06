09d84cc3c08d30f3c0cef2564c63c9802f755bb3
hello7s <strugglemonkey@gmail.com>
2015-10-06 14:30:03 -0400

    Coercions added
    R course for Coercion exercise

---
+++ Coercions.R
@@ -0,0 +1,22 @@
+# What is the answer to the universe?
+my_numeric <- 42.5
+
+# The quotation marks indicate that the variable is of type character
+my_character <- "some text"
+
+# Change the value of my_logical
+my_logical <- TRUE
+
+# Assign a value to the variable called my_apples
+my_apples <- 5 
+
+# Print out the value of my_apples
+my_apples       
+
+# Assign a value to the variable my_oranges and print it out
+my_oranges <- 6
+my_oranges 
+
+# New variable that contains the total amount of fruit
+my_fruit <- my_apples + my_oranges 
+my_fruit
